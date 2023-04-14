042bf71d1f0f33be42af4e3769e4b859878f7f1f
NastyaLisa <lis.nastiona@gmail.com>
2023-04-14 18:07:41 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,67 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, NgModel, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgModel, Validators } from '@angular/forms';
 import { UserService } from '../services/json-reader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'home-work';
 
-    // submit(form: any){
-    // if(form.valid){
-    //   console.log(form.value);
-    //   } else {
-    //    form.control.markAllAsTouched()
-    //   }
-    //  }
+  // submit(form: any){
+  // if(form.valid){
+  //   console.log(form.value);
+  //   } else {
+  //    form.control.markAllAsTouched()
+  //   }
+  //  }
 
-    users: any[] = [];
+  users: any[] = [];
 
-    constructor(private userService: UserService) {}
-    form!: FormGroup 
-    ngOnInit(){
-            this.form = new FormGroup({
-             login: new FormControl('', [Validators.required,  Validators.pattern('^[a-zA-Z0-9]+$')]),
-             email: new FormControl('', [Validators.required, Validators.email], [this.checkEmail]),
-             password: new FormControl('', [Validators.required, Validators.minLength(7)]),
- 
-            })
-                    this.userService.getUsers().subscribe((data) => {
-                this.users = data;
-              });
-    }
-    checkEmail = (control: any): Promise<any> => {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          const userEmail = control.value;
-          const userExists = this.users.some(user => user.email === userEmail);
-    
-          if (userExists) {
-            control.markAllAsTouched();
-            resolve({ uniqEmail: true });
-          } else {
-            resolve(null);
-          }
-        }, 2000);
-      });
-    }
-    
-  
-     
-     
-    submit(){
-     if(this.form.valid){
+  constructor(private userService: UserService) {}
+  form!: FormGroup;
+  ngOnInit() {
+    this.form = new FormGroup({
+      login: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9]+$'),
+      ]),
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email],
+        [this.checkEmail]
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+  checkEmail = (control: any): Promise<any> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const userEmail = control.value;
+        const userExists = this.users.some((user) => user.email === userEmail);
+
+        if (userExists) {
+          control.markAllAsTouched();
+          resolve({ uniqEmail: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  };
+  submit() {
+    if (this.form.valid) {
       console.log(this.form.value);
-     } else {
-      this.form.markAllAsTouched()
-     }
-     }
-    
-
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
--- src/services/json-reader.service.ts
+++ src/services/json-reader.service.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private usersUrl = 'assets/users.json';
 
   constructor(private http: HttpClient) {}
-  
+
   getUsers(): Observable<any> {
     return this.http.get<any>(this.usersUrl);
   }
